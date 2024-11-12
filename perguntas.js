0f21a78e9d8a10de8a1524add3c1a794a622bc86
gh0ststudent <buhoge.k@gmail.com>
2024-11-11 21:57:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futbol',
+    'Quantos jogadores tem um time?',
+    'São 11 jogadores'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlin'
+)
+
+criaCartao(
+    'Matematica',
+    'Quanto é 7+8x0-2',
+    'O resultado é -2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz comer em Inglês?',
+    'Fast em ingles é Eat'
+)
